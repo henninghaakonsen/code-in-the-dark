6125d539f5c40c446bed53c1f9dffc2bb890fd0f
Henning Håkonsen <henninghaakonsen@me.com>
2019-04-24 18:25:07 +0200

    Oppdatert base filene med program, URL for gif og resultat screenshot.

---
+++ src/constants.js
@@ -0,0 +1,41 @@
+export const program = {
+    tech: {
+        tittel: "Track 1",
+        aktiviteter: [
+            { tidspunkt: "17:40 - 18:05", aktivitet: "?" },
+            {
+                tidspunkt: "18:15 - 18:45",
+                aktivitet:
+                    "Kotlin, ett digg programmeringsspråk. CSS på en måte du ikke trodde gikk an."
+            },
+            {
+                tidspunkt: "18:55 - 19:20",
+                aktivitet: "Å skrive bedre brukeropplevelser"
+            },
+            { tidspunkt: "19:30 - 19:45", aktivitet: "Konseptarbeid på speed" }
+        ]
+    },
+    interactive: {
+        tittel: "Track 2",
+        aktiviteter: [
+            { tidspunkt: "17:40 - 18:05", aktivitet: "?" },
+            { tidspunkt: "18:15 - 18:45", aktivitet: "Nye vyer" },
+            {
+                tidspunkt: "18:55 - 19:20",
+                aktivitet:
+                    "Speil, speil på veggen der. 20 ting du ikke visste om JavaScript."
+            }
+        ]
+    },
+    sosialt: {
+        tittel: "Sosialt",
+        aktiviteter: [
+            { tidspunkt: "20:00 - 20:15", aktivitet: "Maxitaxi til Dattera" },
+            { tidspunkt: "20:15 - 21:00", aktivitet: "Middag" },
+            { tidspunkt: "21:00 - sent", aktivitet: "Mingling" }
+        ]
+    }
+};
+
+export const GIF_SOURCE =
+    "https://media.giphy.com/media/DfSLII45H40RW/giphy-downsized.gif";
